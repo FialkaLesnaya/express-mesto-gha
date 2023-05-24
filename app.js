80c04f7878173143e463e0ed0369e0f62f0bb124
Fialka Lesnaya <listik.vs@gmail.com>
2023-05-25 01:15:24 +0300

    Создание роутера и запроса к юзерам

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const PORT = 3000;
 
 const app = express();
-mongoose.connect("mongodb://localhost:27017/mestodb", {
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://127.0.0.1/mestodb", {
   useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
 });
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: "646e8c25df6918733922ab22", // вставьте сюда _id созданного в предыдущем пункте пользователя
+  };
+
+  next();
+});
+
+app.use("/users", require("./routes/users"));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const { createUser, getUsers, getUserById } = require("../controllers/users");
+
+router.get("/", getUsers);
+router.post("/", createUser);
+router.get("/:userId", getUserById);
+
+module.exports = router;
