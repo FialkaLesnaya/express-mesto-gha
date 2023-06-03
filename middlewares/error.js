ca4879fc466805213296fb7f508c94e0b3b9700c
Fialka Lesnaya <listik.vs@gmail.com>
2023-06-03 17:19:16 +0300

    Ошибки

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,11 +1,11 @@
 module.exports.errorMiddleware = (err, req, res, next) => {
   if (err.name === 'ValidationError') {
-    return res.status(400).send({ error: 'Ошибка валидации данных' });
+    return res.status(400).send({ message: 'Ошибка валидации данных' }).then(() => next());
   }
 
   if (err.code === 11000) {
-    return res.status(409).send({ error: 'Пользователь с таким email уже существует' });
+    return res.status(409).send({ message: 'Пользователь с таким email уже существует' }).then(() => next());
   }
 
-  return res.status(500).send({ error: 'Внутренняя ошибка сервера' });
+  return res.status(500).send({ message: 'Внутренняя ошибка сервера' }).then(() => next());
 };
