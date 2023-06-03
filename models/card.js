6740b7df9d7b280cd68bb89aee83888c5674a965
Fialka Lesnaya <listik.vs@gmail.com>
2023-06-03 16:58:15 +0300

    Валидируйте данные на уровне схемы

--- models/card.js
+++ models/card.js
@@ -10,6 +10,12 @@ const cardSchema = new mongoose.Schema({
   link: {
     type: String,
     required: true,
+    validate: {
+      validator(v) {
+        return /^https?:\/\/(?:www\.)?[a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]+$/.test(v);
+      },
+      message: (props) => `${props.value} данное значение не соответствует формату ссылки`,
+    },
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
