3be86749082b2b787f44d6968a00e26734618c8c
Fialka Lesnaya <listik.vs@gmail.com>
2023-06-08 01:49:09 +0300

    Токен

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../utils/config');
 const {
   AUTH_ERROR_CODE,
 } = require('../utils/utils');
@@ -12,13 +13,13 @@ module.exports.authMiddleware = (req, res, next) => {
 
   try {
     // console.log('token', token);
-    const payload = jwt.verify(token, 'jwt');
+    const payload = jwt.verify(token, JWT_SECRET);
     // console.log('token2', token);
     req.headers.authorization = token;
     req.user = payload;
 
     return next();
   } catch (error) {
-    return res.status(AUTH_ERROR_CODE).send({ message: `Недействительный токен авторизации ${req.headers.authorization} или ${req.body.token} или ${req.cookies.token}` });
+    return res.status(AUTH_ERROR_CODE).send({ message: `Недействительный токен авторизации // ${req.headers.authorization} или ${req.body.token} или ${req.cookies.token}` });
   }
 };
--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,3 @@
 module.exports.PORT = '3000';
-module.exports.JWT_SECRET = 'jwt';
+module.exports.JWT_SECRET = 'JWT_SECRET_KEY';
 module.exports.DB_ADDRESS = 'mongodb://127.0.0.1/mestodb';
