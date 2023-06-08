091dfe28697f447889999baff49d57fa08b64431
Fialka Lesnaya <listik.vs@gmail.com>
2023-06-08 03:06:29 +0300

    Исправление ошибок в app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const { errors, celebrate } = require('celebrate');
 const { NOT_FOUND_ERROR_CODE } = require('./utils/utils');
 const { errorMiddleware } = require('./middlewares/error');
@@ -18,6 +20,15 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+
+// Apply the rate limiting middleware to all requests
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+}));
 
 mongoose.connect(DB_ADDRESS, {
   useNewUrlParser: true,
@@ -31,11 +42,12 @@ app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
 app.use(errors());
-app.use(errorMiddleware);
-
-app.use((_, res) => {
-  res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Путь неизвестен' });
+app.use((_, __, next) => {
+  const error = new Error();
+  error.code = NOT_FOUND_ERROR_CODE;
+  next(error);
 });
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
